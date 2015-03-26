8d8003f52cf82a7b7ba875ef4639308d87b0e03e
Arkadiusz Ziółkowski <209296@student.pwr.wroc.pl>
2015-03-26 21:40:45 +0100

    Srawozdanie

--- prj/src/main.cpp
+++ prj/src/main.cpp
@@ -44,19 +44,19 @@
 #define ILOSC_PROB 3
 
 int main(int argc, char *argv[]) {
-  unsigned int iloscDanych[ILOSC_PROB] = {1, 10, 1000};
-  std::string nazwaPlikuStat[3] = {"LinkLista.dat" , "ListArr1.dat", "ListArr2x.dat"};
+  unsigned int iloscDanych[ILOSC_PROB] = {10, 25000, 75000};
+  std::string nazwaPlikuStat[3] = {"LinkLista.dat", "ListArr2x.dat", "ListArr1.dat"};
   Framework *I;
   Lista<int> *LL = new Lista<int>;
   ListArr1<int> *LA1 = new ListArr1<int>;
   ListArr2x<int> *LA2 = new ListArr2x<int>;
   Benchmark<int> *B = new Benchmark<int>(ILOSC_PROB, iloscDanych, ILOSC_POWTORZEN);
 
-  for(int j=0; j<3; j++) { 
+  for(int j=0; j<2; j++) { 
     switch(j) {
     case 0: I = LL; break;
-    case 1: I = LA1; break;
-    case 2: I = LA2; break;
+    case 1: I = LA2; break;
+    case 2: I = LA1; break;
     default: std::cerr << "Brak przypisania wkaznika" << std::endl;
     }
     B -> Test(I, nazwaPlikuStat[j]);
